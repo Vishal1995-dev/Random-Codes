625032663ce64cafa826d94db683c3b88bd13b07
Vishal1995-dev <70947761+Vishal1995-dev@users.noreply.github.com>
2021-02-05 19:58:27 +0530

    Update Leetcode-77.py

--- Leetcode-77.py
+++ Leetcode-77.py
@@ -9,6 +9,15 @@ class Solution(object):
         for i in range(1,n+1):
             a.append(i)
         
-        from itertools import permutations  
-
-        return combinations(a,k) 
+        def n_length_combo(lst, n):
+            if n == 0: 
+                return [[]] 
+            l =[] 
+            for i in range(0, len(lst)): 
+                m = lst[i] 
+                remLst = lst[i + 1:]
+                for p in n_length_combo(remLst, n-1):
+                    l.append([m]+p) 
+            return l
+        
+        return n_length_combo(a,k)
