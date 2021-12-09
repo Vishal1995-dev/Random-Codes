eb3706786d9524d44d1daab37c1c4059a8b5455d
Vishal1995-dev <70947761+Vishal1995-dev@users.noreply.github.com>
2021-12-09 11:54:55 +0530

    Range Sum of Sorted Subarray Sums

---
+++ Leetcode-1508.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rangeSum(self, nums, n, left, right):
+        """
+        :type nums: List[int]
+        :type n: int
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        arr=[]
+        for i in range(n):
+            s=0
+            for j in range(i,n):
+                s+=nums[j]
+                arr.append(s)
+        arr.sort()
+        return sum(arr[left-1:right])%1000000007
