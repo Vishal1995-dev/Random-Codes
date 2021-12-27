2942d249fd3e8bb2fd82954a69a3b2412816a28b
Vishal1995-dev <70947761+Vishal1995-dev@users.noreply.github.com>
2021-12-27 17:53:11 +0530

    Three Divisors

---
+++ Leetcode-1952.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        cnt=0
+        for i in range(1,n+1):
+            if(n%i==0):
+                cnt+=1
+            if(cnt>3):
+                return False
+        return cnt==3
