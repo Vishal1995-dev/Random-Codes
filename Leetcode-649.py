e8f9bc665e746ee741b313d4fc590ec1fe2dbb90
Vishal1995-dev <70947761+Vishal1995-dev@users.noreply.github.com>
2022-03-31 18:11:42 +0530

    Dota2 Senate

---
+++ Leetcode-649.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        """
+        :type senate: str
+        :rtype: str
+        """
+        l=len(senate)
+        d=collections.deque()
+        r=collections.deque()
+        for i,c in enumerate(senate):
+            if(c=='R'):
+                r.append(i)
+            else:
+                d.append(i)
+        while(r and d):
+            p=r.popleft()
+            q=d.popleft()
+            if(p<q):
+                r.append(p+l)
+            else:
+                d.append(q+l)
+        if(r):
+            return 'Radiant'
+        else:
+            return 'Dire'
